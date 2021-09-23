53b50fbd34c0a920baf403311a0e1a76fdb59dc7
shubhammore27 <shubham.more@kanakasoftware.com>
2021-09-23 16:02:10 +0530

    0005

--- src/app/teamdetails.service.ts
+++ src/app/teamdetails.service.ts
@@ -1,9 +1,23 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TeamdetailsService {
+  // apiUrl = 'https://docs.google.com/spreadsheets/d/${spreadsheetId}/gviz/tq?tqx=out:json'
+  apiUrl = 'https://docs.google.com/spreadsheets/d/1DHxB7wGNX7EJ8_XrQR3HAHMjhxq2ziBax-7y1NzKrpE/gviz/tq?tqx=out:json'
+
+  
+
+  httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type': 'application/json',
+      'Access-Control-Allow-Origin':'*'
+    }),
+  };
+
+
 
   Teamdetails = [
     {
@@ -11,69 +25,89 @@ export class TeamdetailsService {
       "Team_Name": "Puneri Bana",
       "Team_Points": 2,
       "Team_Logo": "PuneriBana.png",
-      "Team Members": ["Varun Kulkarni", "Chaitali Suryawanshi", "Yogesh Babar"]
+      "Team_Members": ["Varun Kulkarni", "Chaitali Suryawanshi", "Yogesh Babar"]
     },
     {
       "Team_Id": 2,
       "Team_Name": "Pune Phoenix Risers",
       "Team_Points": 0,
       "Team_Logo": "punePhoenixRisers.png",
-      "Team Members": ["Tanmay kulkarni", "Sujit Kharat", "Apratim Gholap"]
+      "Team_Members": ["Tanmay kulkarni", "Sujit Kharat", "Apratim Gholap"]
     },
     {
       "Team_Id": 3,
       "Team_Name": "Puneri Paltan",
       "Team_Points": 1,
       "Team_Logo": "puneriPaltan.png",
-      "Team Members": ["Suraj Kalbhor", "Rajendra Garade", "Tejas Dhande"]
+      "Team_Members": ["Suraj Kalbhor", "Rajendra Garade", "Tejas Dhande"]
     },
     {
       "Team_Id": 4,
       "Team_Name": "Puneri Fighters",
       "Team_Points": 1,
       "Team_Logo": "PuneriFighter.png",
-      "Team Members": ["Shweta Ranpise", "Ujjwal Bhattacharyya", "Prasad Muley"]
+      "Team_Members": ["Shweta Ranpise", "Ujjwal Bhattacharyya", "Prasad Muley"]
     },
     {
       "Team_Id": 5,
       "Team_Name": "Pune Peshwas",
       "Team_Points": 1,
       "Team_Logo": "punePeshwas.png",
-      "Team Members": ["Mangesh Kulkarni", "Atharva Bhatkhalkar", "Shubham Kamble", "Krishna Kawade"]
+      "Team_Members": ["Mangesh Kulkarni", "Atharva Bhatkhalkar", "Shubham Kamble", "Krishna Kawade"]
     },
     {
       "Team_Id": 6,
       "Team_Name": "Pune Warriors",
       "Team_Points": 1,
       "Team_Logo": "puneWarriors.png",
-      "Team Members": ["Amey Taksale", "Yash Abhyankar", "Rahul Chavan"]
+      "Team_Members": ["Amey Taksale", "Yash Abhyankar", "Rahul Chavan"]
     },
     {
       "Team_Id": 7,
       "Team_Name": "Kanaka Strikers",
       "Team_Points": 0,
       "Team_Logo": "kanakaStrikers.png",
-      "Team Members": ["Nikita Pingle", "Diptee Hamdapurkar", "Chetan Mahore"]
+      "Team_Members": ["Nikita Pingle", "Diptee Hamdapurkar", "Chetan Mahore"]
     },
     {
       "Team_Id": 8,
       "Team_Name": "Game Swingers",
       "Team_Points": 0,
       "Team_Logo": "gameSwingers.png",
-      "Team Members": ["Nitin Shirsat", "Akshay Misal", "Aishwarya Sukre"]
+      "Team_Members": ["Nitin Shirsat", "Akshay Misal", "Aishwarya Sukre"]
     },
     {
       "Team_Id": 9,
       "Team_Name": "Xtreme Pune",
       "Team_Points": 1,
       "Team_Logo": "XtremePune.png",
-      "Team Members": ["Navratan Jangid", "Akshay Bhagat", "Shubham More"]
+      "Team_Members": ["Navratan Jangid", "Akshay Bhagat", "Shubham More"]
     }
   ]
+
+  getSheetData(){
+ 
+    let promise = new Promise((resolve, reject) => {
+      this.http.get(this.apiUrl, this.httpOptions).subscribe((data: any) => {
+        if (data?.Error) {
+          reject(data)
+        } else {
+          resolve(data)
+        }
+      });
+    });
+    return promise;
+  }
+
+  // fetch(`https://docs.google.com/spreadsheets/d/${spreadsheetId}/gviz/tq?tqx=out:json`)
+  //   .then(res => res.text())
+  //   .then(text => {
+  //       const json = JSON.parse(text.substr(47).slice(0, -2))
+  //   })
 
   getTeamDeda(){
     return this.Teamdetails
   }
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 }
