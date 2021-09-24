40c4d28872139c17d9aa67f19c49522131a3915b
unknown <varun.kulkarni@kanakasoftware.com>
2021-09-24 11:17:08 +0530

    made changes and fixed to date

--- src/app/Pipes/getteaminfo.pipe.ts
+++ src/app/Pipes/getteaminfo.pipe.ts
@@ -5,14 +5,24 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class GetteaminfoPipe implements PipeTransform {
 
+  TodaysDate = new Date().setHours(0, 0, 0, 0);
 
-  transform( value: any, team: any ): any {
-    if(!value || !team){
+  transform(value: any, team: any): any {
+    if (!value || !team) {
       return
     }
-    else{ 
-      return  team.filter((s: any) => s.Team_NAME==value.toString());
+    else {
+
+      return value.filter((s: any) => {
+        let TeamSubmissionDate = new Date(s.TIME_STAMP).setHours(0, 0, 0, 0);
+        if (s.Team_ID == team.toString() && this.TodaysDate == TeamSubmissionDate) {
+          return true;
+        } else {
+          return false;
+        }
+      });
     }
+
   }
 
 }
--- src/app/cards/cards.component.ts
+++ src/app/cards/cards.component.ts
@@ -8,14 +8,13 @@ import { TeamdetailsService } from '../Services/teamdetails.service';
 })
 export class CardsComponent implements OnInit {
 
-  teamDetails: any
-  todaysTeam = []
-  popUpSelectedTeam: any;
-  teamname: any;
-  selectedTeam: any = {}
-  headingName
-  objSize
-  constructor(private _TeamdetailsService: TeamdetailsService) { }
+  teamDetails : any
+  todaysTeam : any
+  popUpSelectedTeam : any;
+  teamname:any;
+  
+
+  constructor(private _TeamdetailsService : TeamdetailsService) { }
 
   ngOnInit(): void {
     this.teamDetails = this._TeamdetailsService.getTeamDeda().sort((a, b) => {
@@ -25,38 +24,19 @@ export class CardsComponent implements OnInit {
 
   }
 
-  update(team: any) {
-    this.getData()
-    this.headingName = team
-    console.log('selected ==>',team);
-    this.selectedTeam = {}
-    this.todaysTeam.forEach((item) => {
-      console.log('conditional',item.Team_NAME.includes(team));
-      
-      if (item.Team_NAME.includes(team)) {
-        this.selectedTeam = item
-      }
-    })
 
-    this.objSize = Object.keys(this.selectedTeam).length
-  }
+  getData(){
+    this._TeamdetailsService.getSheetData().then((res:any)=>{
+      let x=[];
+      for (var team of res){
+        let y={ 
+   "TIME_STAMP":team.c[0].f,
+		"Team_ID":team.c[1].v.split(' ')[0],
+    "Team_MOM":team.c[2].v,
+    "Team_BTS": team.c[3].v,
+    "Team_BOW": team.c[4].v}
 
-
-  getData() {
-    this._TeamdetailsService.getSheetData().then((res: any) => {
-      // console.log(res);
-      
-      let x = [];
-      let y
-      for (var team of res) {
-
-        y = {
-          "TIME_STAMP": team.c[0].f,
-          "Team_NAME": team.c[1].v,
-          "Team_MOM": team.c[2].v,
-          "Team_BTS": team.c[3].v,
-          "Team_BOW": team.c[4].v
-        }
+  
 
         x.push(y)
 
